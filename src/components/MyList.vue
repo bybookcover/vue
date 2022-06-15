e59e4d367fbbdb41fd3e4ff430ea35eae1ec1f3b
binyang <bybookcover@hotmail.com>
2022-06-15 16:14:53 +0800

    main fixed

---
+++ src/components/MyList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <ul class="todo-main">
+      <MyItem></MyItem>
+      <MyItem></MyItem>
+    </ul>
+  </div>
+</template>
+
+<script>
+import MyItem from './MyItem.vue';
+
+export default {
+    name: "MyList",
+    components: { MyItem }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+
+  /*main*/
+  .todo-main {
+    margin-left: 0px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0px;
+  }
+
+  .todo-empty {
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+  }
+</style>
