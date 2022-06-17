30f5d9d748abf4708c4c6ae899f407b7c39c7b1e
binyang <bybookcover@hotmail.com>
2022-06-17 16:52:18 +0800

    main

---
+++ src/components/Student.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="student">
+    <h2>学生名称：{{name}}</h2>
+    <h2>学生性别：{{sex}}</h2>
+  </div>
+</template>
+
+<script>
+export default{
+  name:'Student',
+  data () {
+    return {
+      name:'张三',
+      sex:'男'
+    }
+  }
+}
+</script>
+<style>
+.student{
+  background-color: pink;
+  margin:5px;
+  padding: 5px;
+  margin-top: 20px;
+}
+</style>
