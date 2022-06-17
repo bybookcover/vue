372a24d65e54acb047eed08292ec6f895b1f6789
binyang <bybookcover@hotmail.com>
2022-06-18 05:47:36 +0800

    自定义事件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="app">
     <h1 >{{msg}}</h1>
-    <School></School>
-    <Student></Student>
+    <School :getSchoolName="getSchoolName"></School>
+       <Student v-on:atguigu="demo" @m1bind="m1"></Student>
+    <!-- <Student ref="student"></Student> -->
   </div>
 </template>
 
@@ -16,12 +17,29 @@ export default {
   components: {
     School,
     Student
-},
+  },
   data () {
     return {
       msg:'你好呀'
     }
-  }
+  },
+  methods:{
+    getSchoolName(name){
+      console.log('收到了school的信息',name)
+    },
+    demo(){
+      console.log('demo被调用了')
+    },
+    m1(){
+      console.log('m1被调用了')
+    }
+  },
+  // mounted(){
+  //   setTimeout(() => {
+  //         this.$refs.student.$on('atguigu',this.demo)
+  //   }, 3000);
+
+  // }
 }
 </script>
 
--- src/components/Student.vue
+++ src/components/Student.vue
@@ -2,6 +2,8 @@
   <div class="student">
     <h2>学生名称：{{name}}</h2>
     <h2>学生性别：{{sex}}</h2>
+    <button @click="sendStudentName">把学生名给app</button>
+    <button @click="unbind">解绑atguigu</button>
   </div>
 </template>
 
@@ -12,6 +14,17 @@ export default{
     return {
       name:'张三',
       sex:'男'
+    }
+  },
+  methods:{
+    sendStudentName(){
+      this.$emit('atguigu',this.name)
+      this.$emit('m1bind')
+    },
+    unbind(){
+      // this.$off('atguigu') //解绑一个
+      // this.$off(['atguigu','m1bind']) // 解绑多个
+      this.$off() //解绑全部
     }
   }
 }
