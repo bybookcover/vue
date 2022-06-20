e563910452e86f84dfdac8f57adbb823f7c0062c
binyang <bybookcover@hotmail.com>
2022-06-20 16:49:29 +0800

    vuex

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,14 @@
 <template>
-    <div class="container">
-     <Category title="美食" >
-      <template scope="atguigu">
-            <ul >
-      <li v-for="(item,index) in atguigu.games" :key="index">{{item}}</li>
-     </ul>
-      </template>
-     </Category>
-
-    <Category title="美食" >
-      <template scope="atguigu">
-            <ol >
-      <li v-for="(item,index) in atguigu.games" :key="index">{{item}}</li>
-     </ol>
-      </template>
-     </Category>
-          
-        <Category title="美食" >
-                  <!-- //解构赋值方法 {games} ==atguigu.games -->
-      <template scope="{games}"> 
-
-         
-      <h4 v-for="(item,index) in games" :key="index">{{item}}</h4>
-
-      </template>
-     </Category>
-    </div>
+  <div id="app">
+    <Count></Count>
+  </div>
 </template>
 
 <script>
-import Category from './components/Category';
+import Count from './components/Count.vue';
 export default {
     name: "App",
-    components: { Category }
+    components: { Count }
     
 }
   
---
+++ src/store/index.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    sum:0,
+    school:'sgg',
+    subject:'前端'
+  },
+  getters: {
+  },
+  mutations: {
+    JIA(state,value){
+      state.sum += value
+      console.log('mutation+jia被调用了')
+    },
+    JIAN(state,value){
+      state.sum -= value
+      console.log('mutation-jian被调用了')
+    }
+  },
+  actions: {
+    jia(context,value){
+      console.log('jia被调用了')
+      context.commit('JIA',value)
+    },
+    jian(context,value){
+      console.log('jian被调用了')
+      context.commit('JIAN',value)
+    },
+    jiaOdd(context,value){
+      console.log('奇数被调用了')
+      if(context.state.sum%2)
+      context.commit('JIA',value)
+    },
+    jiaWait(context,value){
+      console.log('wait被调用了')
+      setTimeout(() => {
+      context.commit('JIA',value)
+      }, 500);
+    }
+  },
+  modules: {
+  }
+})
