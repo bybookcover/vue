ffa65b78d7e1cb68316ec78b7ee3f5e7aeddeed9
binyang <bybookcover@hotmail.com>
2022-06-22 16:09:38 +0800

    router

---
+++ 24-模块化 vuex/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Count></Count>
+    <hr>
+    <Person></Person>
+  </div>
+</template>
+
+<script>
+import Count from './components/Count.vue';
+import Person from './components/Person.vue';
+export default {
+    name: "App",
+    components: { Count, Person }
+    
+}
+  
+</script>
+
+<style >
+#app{
+  padding: 5px;
+}
+.container,.foot{
+  display: flex;
+  justify-content: space-around;
+}
+ 
+</style>
